5aa69b8a4f9944250eeeadf2aadb1751f4a5308f
vatsalyaroop1184 <vatsalyaroopchalumuri@gmail.com>
2026-08-23 22:44:16 +0530

    first commit on linked and double linked lists basic functions

---
+++ double_linked_lists/deletions.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node* prev;
+    struct node* next;
+};
+
+struct node* create_node(int value){
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = value;
+    newnode->next = NULL;
+    newnode->prev = NULL;
+    return newnode;
+}
+
+struct node* insert_at_end(int value,struct node* main_header){
+    struct node* newnode = create_node(value);
+      struct node* header = main_header;
+   if(header == NULL){
+    return newnode;
+   }
+      
+    while(header->next != NULL){
+       header = header->next;
+    }
+    header->next = newnode;
+    newnode->prev = header;
+
+    return main_header;
+}
+
+struct node* insert_at_front(int value,struct node* header){
+    struct node* newnode = create_node(value);
+    if(header == NULL){
+        return newnode;
+    }
+    newnode->next = header;
+    header->prev = newnode;
+    return newnode;
+}
+
+struct node* del_at_end(struct node* header){
+    struct node* newnode = header;
+    while(newnode->next->next != NULL){
+        newnode = newnode->next;
+    }
+    free(newnode->next);
+    newnode->next = NULL;
+    
+    return header;
+}
+
+struct node* del_at_front(struct node* header){
+    struct node* temp = header->next;
+     free(header);
+    header = NULL;
+    return temp;
+
+}
+
+
+
+
+
+void print_list(struct node* header){
+    struct node* temp = header;
+    while(temp->next != NULL){
+
+    printf("%d->",temp->data);
+    temp = temp->next;
+        if(temp->next == NULL){
+        printf("%d",temp->data);
+    }
+    
+    }
+   
+    
+}
+
+void free_list(struct node* header){
+    struct node* temp = header;
+    while(header != NULL){
+        temp = header;
+        header = header->next;
+        free(temp);
+    }
+}
+
+int main(){
+    struct node* n1 = NULL;
+    struct node* n2 = NULL;
+    int t;
+    while(1){
+        scanf("%d",&t);
+        if(t == -1){
+            break;
+        }
+        else{
+            n1 = insert_at_end(t,n1);
+            n2 = insert_at_front(t,n2);
+        }
+    }
+    int p = 2;
+    while(p){
+       n1 = del_at_end(n1);
+       n2 = del_at_front(n2);
+       p--;
+    }
+    print_list(n1);
+    printf("\n");
+    print_list(n2);
+    free_list(n1);
+    free_list(n2);
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ double_linked_lists/readme.md
@@ -0,0 +1,4 @@
+Here i in some deletions and insertions functions i didnt consider edge cases like when header has only one node or no node at all.
+
+-> think where u can use "node->prev" also efficiently like we can use it in del_at_end function.
+cd
